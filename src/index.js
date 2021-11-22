23d2c2256fe783270232f16c2c5d137ce6bd67f5
Lokesh <lokesh.bhoyar>
2021-11-23 02:22:16 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './main/app/App';
+import reportWebVitals from './test/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
